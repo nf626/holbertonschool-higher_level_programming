23070d32d5bc6ac75e49fba8196fed46356dc4b9
Nigel Feng <10083@holbertonstudents.com>
2025-01-16 20:18:49 +1100

    3. When I was having that alphabet soup, I never thought that it would pay off

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ASCII alphabet lowercase except q and e"""
 for i in range(ord('a'), ord('z') + 1):
-    if i == 'e' or i == 'q':
+    if i == ord('e') or i == ord('q'):
         continue
     else:
         print('{:c}'.format(i), end='')
