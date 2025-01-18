38c671bc38d77fa7cd8514987666737ca176e6ae
Nigel Feng <10083@holbertonstudents.com>
2025-01-18 16:11:03 +1100

    8. To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def uppercase(s):
+def uppercase(str):
     """makes all characters in a string uppercase."""
-    for c in s:
+    for c in str:
         if (ord(c) >= 97 and ord(c) <= 122):
             c = chr(ord(c) - 32)
         print('{0}'.format(c), end="")
