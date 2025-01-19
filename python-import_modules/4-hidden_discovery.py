841db3aa182ddb42a3c8778a4bcc7ba53e44f326
Nigel Feng <10083@holbertonstudents.com>
2025-01-19 03:02:14 +0000

    4. Who are you?

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import hidden_4
+
+    hidden_name = dir(hidden_4)
+
+    for name in hidden_name:
+        if name[:2] != "__":
+            print("{}".format(name))
