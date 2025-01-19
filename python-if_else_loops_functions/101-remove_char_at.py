72292699e7c7ccef94e0be6466160de2a6c572c4
Nigel Feng <10083@holbertonstudents.com>
2025-01-19 17:42:28 +1100

    14. Remove at position

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+creates a copy of the string, 
+removing the character at the position n
+"""
+def remove_char_at(str, n):
+    if n < 0:
+        return str
+    else:
+        str = str[:n] + str[n + 1:]
+        return str
