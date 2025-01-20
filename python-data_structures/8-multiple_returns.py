f5c9ecd840b3efde49224543426ab185ba195d6a
Nigel Feng <10083@holbertonstudents.com>
2025-01-21 10:27:25 +1100

    8. More returns!

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    
+    new_string = tuple()
+
+    if len(sentence) == 0:
+        new_string = ('None',)
+        return (len(new_string), new_string[0])
+    else:
+        return (len(sentence), sentence[0])
