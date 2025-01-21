cbf4b9653342262696c7789f44fa9a485a1c9693
Nigel Feng <10083@holbertonstudents.com>
2025-01-22 00:13:52 +1100

    2. Unique addition

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    new_list = list(set(my_list))
+    new_list = set(my_list)
     total = 0
     for i in new_list:
-        total = total + i
+       total = total + i
     return (total)
