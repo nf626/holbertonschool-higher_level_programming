acf40fabf57f8f8057760ab41f28eae09ed56000
Nigel Feng <10083@holbertonstudents.com>
2025-01-22 01:00:05 +1100

    6. Print sorted dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary.keys())
     for key in sorted_dict:
-        print(f"{key} {a_dictionary[key]}")
+        print(f"{key}: {a_dictionary[key]}")
