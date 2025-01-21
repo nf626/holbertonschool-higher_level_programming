e747895946774cd09648671793451edd764cb410
Nigel Feng <10083@holbertonstudents.com>
2025-01-21 23:44:22 +1100

    1. Search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,7 +4,8 @@ def search_replace(my_list, search, replace):
     new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
 
-#for x in a:
+#
+# for x in a:
 #    if x == 30:
 #        b.append(99)
 #    else:
