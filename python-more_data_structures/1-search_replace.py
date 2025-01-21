e962c36405f854a47081b6d49b879e4497f71ab5
Nigel Feng <10083@holbertonstudents.com>
2025-01-21 23:42:57 +1100

    1. Search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,10 +3,13 @@
 def search_replace(my_list, search, replace):
     new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
-"""
+
+'''
+
+
 for x in a:
     if x == 30:
         b.append(99)
     else:
         b.append(x)
-"""
+'''
