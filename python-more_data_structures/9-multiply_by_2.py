3af916bfbacb637d682499d1590a58b589e562bf
Nigel Feng <10083@holbertonstudents.com>
2025-01-22 12:26:38 +1100

    9. Multiply by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    double_dictionary = {key:value * 2 for (key, value) in a_dictionary.items()}
-    return double_dictionary
+    double_dict = {key: value*2 for (key, value) in a_dictionary.items()}
+    return double_dict
