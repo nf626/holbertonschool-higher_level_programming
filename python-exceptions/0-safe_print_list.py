4d92b4a1e4dca6ce8c402e272dbc6273c4ab9397
Nigel Feng <10083@holbertonstudents.com>
2025-01-23 13:20:41 +1100

    0. Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{my_list[i]}", end='')
             num = num + 1
-        except:
+        except Exception:
             None
     print()
     return (num)
