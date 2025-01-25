434b842023cd9d00e820aef8ea66e6021890a2e8
Nigel Feng <10083@holbertonstudents.com>
2025-01-25 23:14:55 +1100

    1. Divide a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -21,9 +21,6 @@ def matrix_divided(matrix, div):
             matrix should be divided by div,
             rounded to 2 decimal places.
     """
-    if (not isinstance(matrix, list) or matrix == [] or not all(isinstance(row, list)) for row in matrix) or not all((isinstance(values, int) or isinstance(values, float)) for values in [coloumn for row in matrix for coloumn in row]):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
