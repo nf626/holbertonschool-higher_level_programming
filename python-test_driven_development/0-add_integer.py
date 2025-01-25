a959c56dfbc21780f71ab9b9b8548b06e1ac0cc5
Nigel Feng <10083@holbertonstudents.com>
2025-01-25 17:55:36 +1100

    0. Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Module: Add two integers"""
 
+
 def add_integer(a, b=98):
     """
-    
-    - Function adds two integers.
-    - Raise a TypeError exception if a and b not integer or float
-    - Returns an integer: the addition of a and b
+
+    Function adds two integers.
+
+    Raise a TypeError exception
+    if a and b not integer or float.
+
+    Returns an integer: the addition of a and b.
     """
     total = 0
     if (not isinstance(a, float) and not isinstance(a, int)):
