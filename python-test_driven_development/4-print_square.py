99abcc3a368b5194f07f3e168b8c1a286f49a001
Nigel Feng <10083@holbertonstudents.com>
2025-01-26 00:04:21 +1100

    3. Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module: Print square"""
+
+
+def print_square(size):
+    """
+    
+    Function that prints a square with the character #.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if isinstance(size, float) < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print(f"{'#'}", end='')
+        print("")
