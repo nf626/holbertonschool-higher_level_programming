2527b6392616b4f6642291d78347316142772cd3
Nigel Feng <10083@holbertonstudents.com>
2025-01-26 16:53:46 +1100

    5. Max integer - Unittest

--- python-test_driven_development/main.py
+++ python-test_driven_development/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-text_indentation = __import__('5-text_indentation').text_indentation
+max_integer = __import__('6-max_integer').max_integer
 
-text_indentation()
+print(max_integer([1, 2, 3, 4]))
+print(max_integer([1, 3, 4, 2]))
--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
-'''Unittests for max_integer'''
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """Test for max integer"""
+    def test_max_integer(self):
+        """max int is 4"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_int_beginning(self):
+        """max int found in beginning"""
+        self.assertEqual(max_integer([10, 2, 3, 4]), 10)
+
+    def test_max_int_middle(self):
+        """max int found in middle"""
+        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
+
+    def test_max_int_negative(self):
+        """max int found in middle"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_max_int_one_negative(self):
+        """max int found in middle"""
+        self.assertEqual(max_integer([1, 2, 3, -4]), 3)
