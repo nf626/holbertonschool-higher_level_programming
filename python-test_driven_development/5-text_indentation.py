b1c45c544c693e70165c9ca8bda3bc50c89d2e70
Nigel Feng <10083@holbertonstudents.com>
2025-01-26 12:30:41 +1100

    4. Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,4 +14,13 @@ def text_indentation(text):
     while (count < len(text) and text[count] == " "):
         count = count + 1
 
-    
+    while count < len(text):
+        print(text[count], end='')
+        if (text[count] == '\n' or text[count] in ".?:"):
+            if text[count] in ".?:":
+                print('\n')
+            count = count + 1
+            while (count < len(text) and text[count] == " "):
+                count = count + 1
+            continue
+        count = count + 1
