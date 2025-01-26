d8f61f317932bd7f2d97ea909fb56e98b29b20e4
Nigel Feng <10083@holbertonstudents.com>
2025-01-26 15:47:09 +1100

    4. Text indentation

--- python-test_driven_development/main.py
+++ python-test_driven_development/main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 text_indentation = __import__('5-text_indentation').text_indentation
 
-text_indentation("Hello: welcome to python. what questions do you have?")
+text_indentation()
