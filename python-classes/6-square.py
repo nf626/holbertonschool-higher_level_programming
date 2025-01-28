a5a8915caa199754f5dd7097536a7783e392ad89
Nigel Feng <10083@holbertonstudents.com>
2025-01-28 12:55:58 +1100

    6. Coordinates of a square

---
+++ python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Access and update private attribute"""
+
+
+class Square:
+    """Class to create square"""
+    __size = None
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """getter method"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """setter method"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return ()
+    @position.setter
+    def position(self, value):
+
+    def area(self):
+        """Calculate area"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints a square"""
+        if self.__size == 0:
+            print("")
+
+        for i in range(self.__size):
+            print("#" * self.__size)
+
