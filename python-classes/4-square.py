8b8b7288161d1fc230103ae853a5f8746d6d971e
Nigel Feng <10083@holbertonstudents.com>
2025-01-28 12:39:21 +1100

    4. Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -13,7 +13,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-    
+
     @property
     def size(self):
         """getter method"""
