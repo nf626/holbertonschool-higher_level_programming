96b753a1f32b2b787f2dcc604260577b812273c8
Nigel Feng <10083@holbertonstudents.com>
2025-02-03 18:29:04 +1100

    6. Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+    
+    def area(self):
+        """Area instance method"""
+        raise Exception("area() is not implemented")
