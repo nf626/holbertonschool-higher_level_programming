793479f09abbe07c8e8db51b02a0da3792dff6c4
Nigel Feng <10083@holbertonstudents.com>
2025-02-03 16:17:37 +1100

    0. Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function that returns the list of 
+    available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """function returns list of 
+        attributes and methods"""
+    return dir(obj)
