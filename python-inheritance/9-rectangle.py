b51872afad09cdb531dc785cd44e9ba5a5b4a56c
Nigel Feng <10083@holbertonstudents.com>
2025-02-03 21:56:12 +1100

    9. Full rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -11,3 +11,10 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Calculate area of rectangle"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
