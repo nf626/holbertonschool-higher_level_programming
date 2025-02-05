eea900f553cc6b2dad442d8ca08596a318df289f
Nigel Feng <10083@holbertonstudents.com>
2025-02-05 13:21:12 +1100

    1. Shapes, Interfaces, and Duck Typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -19,29 +19,33 @@ class Shape(ABC):
 class Circle(Shape):
     """Circle class"""
     def __init__(self, radius):
-        self.radius = radius
+        self.__radius = radius
 
     def area(self):
         """Area method"""
-        return (math.pi * (self.radius ** 2))
+        if self.__radius < 0:
+            raise AssertionError("Perimeter should handle negative radius")
+        return (math.pi * (self.__radius ** 2))
     
     def perimeter(self):
         """Perimeter method"""
-        return (2 * math.pi * self.radius)
+        if self.__radius < 0:
+            raise AssertionError("Perimeter should handle negative radius")
+        return (2 * math.pi * self.__radius)
 
 class Rectangle(Shape):
     """Rectangle class"""
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def area(self):
         """Area method"""
-        return (self.width * self.height)
+        return (self.__width * self.__height)
     
     def perimeter(self):
         """Perimeter method"""
-        return ((self.width * 2) + (self.height * 2))
+        return ((self.__width * 2) + (self.__height * 2))
 
 
 def shape_info(shape_var):
