a395bdda5398af7c5eb61ff0c79686c6178e6946
Nigel Feng <10083@holbertonstudents.com>
2025-02-11 22:06:30 +1100

    5. Save Object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,3 +5,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Save function"""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        save_file = json.dump(my_obj, file)
+        return save_file
