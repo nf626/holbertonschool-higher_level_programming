63c5b51e6cc92ec0dd98c9fa3861ac79cf5ca1f9
Nigel Feng <10083@holbertonstudents.com>
2025-02-12 23:40:13 +1100

    7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """Load, Add, Save module"""
 import sys
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+add_item = "add_item.json"
 
-filename = "add_item.json"
-new_list = sys.argv[1:]
-save_to_json_file(new_list, filename)
-my_list = load_from_json_file(filename)
+try:
+    # Loads file
+    new_obj = load_from_json_file(add_item)
+except FileExistsError:
+    # Check if file exists, does not exist create empty list
+    new_obj = []
+
+new_obj.extend(sys.argv[1:])
+save_to_json_file(new_obj, add_item)
