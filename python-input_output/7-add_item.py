bcb4b6e6d9580c4a7833315ebe37b9544c4ccf89
Nigel Feng <10083@holbertonstudents.com>
2025-02-12 23:41:51 +1100

    7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,8 +9,8 @@ add_item = "add_item.json"
 try:
     # Loads file
     new_obj = load_from_json_file(add_item)
-except FileExistsError:
-    # Check if file exists, does not exist create empty list
+except FileNotFoundError:
+    # Check if file found, does not exist create empty list
     new_obj = []
 
 new_obj.extend(sys.argv[1:])
