0b3e466e158be5721875b91cc1f207c09d194bac
Nigel Feng <10083@holbertonstudents.com>
2025-02-12 11:41:32 +1100

    7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,5 +5,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-my_list = sys.argv
-save_to_json_file(my_list, filename)
+new_list = sys.argv[1:]
+save_to_json_file(new_list, filename)
+my_list = load_from_json_file(filename)
