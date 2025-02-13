fb6993be19abdaf97018e06f6f1a63f1885f177d
Nigel Feng <10083@holbertonstudents.com>
2025-02-13 14:08:44 +1100

    10. Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -12,7 +12,7 @@ class Student:
     def to_json(self, attrs=None):
         """dictionary representation"""
         if (isinstance(attrs, list) and
-            all(isinstance(element, str) for element in attrs)):
+                all(isinstance(element, str) for element in attrs)):
             return {key: getattr(self, key)
-                    for key in attrs if hasattr(self, key)}
+                        for key in attrs if hasattr(self, key)}
         return self.__dict__
