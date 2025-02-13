312e3215d8540686fd68a8d88a79b8473bf25415
Nigel Feng <10083@holbertonstudents.com>
2025-02-14 01:12:09 +1100

    0. Basic Serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,7 +6,8 @@ import json
 def serialize_and_save_to_file(data, filename):
     """Serialise function"""
     with open(filename, mode="w", encoding="utf-8") as file:
-        return json.dump(data, file)
+        json.dump(data, file)
+    print(f"{filename}")
 
 def load_and_deserialize(filename):
     """Deserialise function"""
