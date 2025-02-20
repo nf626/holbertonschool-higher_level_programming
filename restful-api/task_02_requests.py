5a103b0783d4efe80ffc6fe20bd6f870e2f72f91
Nigel Feng <10083@holbertonstudents.com>
2025-02-20 22:49:16 +1100

    fetch_and_save_posts()

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -2,5 +2,8 @@ import requests
 import json
 
 def fetch_and_print_posts():
+    api_url = 'https://jsonplaceholder.typicode.com/todos/1'
+    response = requests.get(api_url)
+
 
 def fetch_and_save_posts():
