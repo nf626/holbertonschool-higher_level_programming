63050e387f767e86b7b3e448edd1c8bc53b91485
Nigel Feng <10083@holbertonstudents.com>
2025-02-21 17:14:20 +1100

    2. Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,11 +6,13 @@ def fetch_and_print_posts():
     """get api data and prints status and data"""
     api_url = 'https://jsonplaceholder.typicode.com/todos/1'
     response = requests.get(api_url, timeout=10)
+
     print("Status Code: {}".format(response.status_code))
+
     if response.status_code == 200:
         data = response.json()
-        for i in data:
-            print(f"{i}")
+        new_list = list(x for x in data)
+        print(new_list)
     else:
         print("Failed to retrieve data".format(response.status_code))
 
