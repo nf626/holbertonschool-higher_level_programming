826221d671b4ee1f259127394144e0c9bd7193c2
Nigel Feng <10083@holbertonstudents.com>
2025-02-21 19:11:47 +1100

    2. Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,9 +1,10 @@
 import json
 import requests
+import csv
 
 
 def fetch_and_print_posts():
-    """get api data and prints status and data"""
+    """get api data and prints status and posts"""
     api_url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(api_url, timeout=5)
 
@@ -17,4 +18,20 @@ def fetch_and_print_posts():
         print("Failed to retrieve data".format(response.status_code))
 
 def fetch_and_save_posts():
-    pass
+    """get api data and store posts"""
+    api_url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(api_url, timeout=5)
+
+    if response.status_code == 200:
+        posts = response.json()
+        for item in posts:
+            post_list_dict = [{"id": item["id"], "title": item["title"], "body": item["body"]}]
+
+        with open("posts.csv", mode="w", encoding="utf-8") as csv_file:
+            field_names = ["id", "title", "body"]
+            writer = csv.DictWriter(csv_file, fieldnames=field_names)
+
+            writer.writeheader()
+            writer.writerows(post_list_dict)
+    else:
+        print("Failed to retrieve data".format(response.status_code))
