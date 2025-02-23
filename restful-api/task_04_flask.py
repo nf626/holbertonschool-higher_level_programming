6c4f59248492e03b7f37c5772b05ad2e22a8d50b
Nigel Feng <10083@holbertonstudents.com>
2025-02-23 17:36:43 +1100

    4. Develop a Simple API using Python with Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    """Home page"""
+    return "Welcome to the Flask API!"
+
+@app.route("/data")
+def serve_json():
+    """json page"""
+    users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+    return jsonify()
+
+@app.route("/status")
+def status_route():
+    """status page"""
+    return "OK"
+
+@app.route("/users/users")
+def user_():
+
+
+if __name__ == "__main__":
+    app.run()
