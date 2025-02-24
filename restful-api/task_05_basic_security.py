7050d6939ac4516549fb5d233fc8ce8ce7ae1d31
Nigel Feng <10083@holbertonstudents.com>
2025-02-24 19:09:44 +1100

    5. API Security and Authentication Techniques

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -3,10 +3,28 @@ from flask import Flask
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
-    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
 }
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+    else:
+        return "Invalid username and password", 401
+
+@app.route("/basic-protected")
+@auth.login_required
+def basic_auth():
+    return "Basic Auth: Access Granted"
+
+if __name__ == "__main__":
+    app.run(debug=True)
