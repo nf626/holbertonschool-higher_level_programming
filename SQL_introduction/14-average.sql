a6ff50c2ec745bd6e9d2cb2a8fe0e71763a86227
Nigel Feng <10083@holbertonstudents.com>
2025-03-05 09:42:04 +1100

    14. Average

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,10 @@
 -- Computes the score average of all records in the
 -- table second_table of the database hbtn_0c_0
-SELECT AVG(score)
-FROM second_table;
+ALTER TABLE second_table
+ADD average INT;
+
+INSERT INTO second_table
+VALUES (
+    SELECT AVG(score)
+    FROM second_table;
+);
