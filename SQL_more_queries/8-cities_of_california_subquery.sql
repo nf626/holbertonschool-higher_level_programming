7f35b5cc5b8254c8d8e2fd893efc60e1ed19f93a
Nigel Feng <10083@holbertonstudents.com>
2025-03-06 23:43:45 +1100

    8. Cities of California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
 -- lists all the cities of California that can be
 -- found in the database hbtn_0d_usa.
-SELECT states
-FROM hbtn_0d_usa
-WHERE name = 'California';
+INSERT INTO hbtn_0d_usa.states
+VALUES (
+    'California'
+);
 ORDER BY cities.id ASC;
