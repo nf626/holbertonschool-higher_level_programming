6f59a4bfc7f323b4d399efd4c0b93d8c39b06e0c
Nigel Feng <10083@holbertonstudents.com>
2025-03-07 12:28:53 +1100

    10. Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- lists all shows contained in hbtn_0d_tvshows
 -- that have at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows
-WHERE tv_shows.genre_id > 0
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
