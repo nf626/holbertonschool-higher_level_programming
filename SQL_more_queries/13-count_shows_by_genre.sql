bbf326c4f2adc76d150c9ff556d580d81fdf2018
Nigel Feng <10083@holbertonstudents.com>
2025-03-07 17:32:16 +1100

    13. Number of shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,5 @@ SELECT tv_genres.name,
 COUNT(*) 'number_of_shows'
 FROM tv_show_genres
 INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP by tv_genres.name
 ORDER BY number_of_shows DESC;
