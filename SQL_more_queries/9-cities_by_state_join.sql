ae350090a5a814ba9de1fd7f6b3f2928a2ed9242
Nigel Feng <10083@holbertonstudents.com>
2025-03-07 11:32:33 +1100

    9. Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.id=states.id;
-ORDER BY cities.id AS C ASC;
+INNER JOIN states ON states.id=cities.id;
+ORDER BY cities.id ASC;
