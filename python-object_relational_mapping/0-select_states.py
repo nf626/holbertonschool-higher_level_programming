4c65ceb3fefeb94c5cab4871bb835fc428ea6358
Nigel Feng <10083@holbertonstudents.com>
2025-03-09 00:49:48 +1100

    0. Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,34 @@
 """ lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 
+if __name__ == '__main__':
 
-MY_HOST = 'localhost'
-MY_USER = 'root'
-MY_DB = 'hbtn_0e_0_usa'
-MY_PORT = 3306
+    # Connect to Database
+    MY_HOST = 'localhost'
+    MY_USER = 'root'
+    MY_DB = 'hbtn_0e_0_usa'
+    MY_PORT = 3306
 
-db = MySQLdb.connect(host=MY_HOST, user=MY_USER, port=MY_PORT, passwd='', db=MY_DB) 
-cur = db.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS states ("
-"id INT NOT NULL AUTO_INCREMENT,"
-"name VARCHAR(256) NOT NULL,"
-"PRIMARY KEY (id))"
-)
-db.close()
+    # Connect method
+    db = MySQLdb.connect(
+        host=MY_HOST,
+        user=MY_USER,
+        port=MY_PORT,
+        passwd='',
+        db=MY_DB
+        ) 
+
+    # Cursor
+    # interact with and manipulate the results of a database query
+    cur = db.cursor()
+
+    # executing SQL queries on the database
+    # SQL commands
+    cur.execute("SELECT * FROM states")
+
+    for state in cur:
+        print(state)
+
+    # Close cursor and database connection
+    cur.close()
+    db.close()
