95405efce37a55a1c3872701d66eb2c147136110
Nigel Feng <10083@holbertonstudents.com>
2025-03-11 12:10:49 +1100

    6. First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement="auto", unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
