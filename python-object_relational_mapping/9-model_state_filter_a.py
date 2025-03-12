535999d1813139bdbd7e70c40cee526d868081c9
Nigel Feng <10083@holbertonstudents.com>
2025-03-12 12:02:20 +1100

    9. Contains `a`

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -4,7 +4,7 @@ the letter a from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import State, Base
 
 
 if __name__ == '__main__':
