797165e13cf5de2b9ce1328ab8bbb7d808c863de
Nigel Feng <10083@holbertonstudents.com>
2025-03-12 17:01:36 +1100

    14. Cities in state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ prints all City objects from the database hbtn_0e_14_usa"""
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, join
+from sqlalchemy.sql import select
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import Base, City
@@ -25,7 +26,7 @@ if __name__ == '__main__':
     session = Session()
 
     # print all city name
-    for city in session.query(City).join(State).order_by(City.id).filter(City.id).all():
+    for city in session.query(City).join(State, State.id == City.state_id, isouter=True).order_by(City.id).filter(City.id).all():
         print(f"{State.name}: ({city.id}) {city.name}")
 
     # Close session
