ac62a09d7bc882a0ae6f70b7934c467156bd1a19
Nigel Feng <10083@holbertonstudents.com>
2025-03-12 11:34:35 +1100

    9. Contains `a`

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""lists all State objects that contain
+the letter a from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+
+if __name__ == '__main__':
+    # Declare connection arguments
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    # starting point
+    engine = create_engine(
+        f"mysql+mysqldb://{user}: {password}@'localhost:3306/{db}"
+    )
+
+    # generates new Session objects when called
+    Session = sessionmaker(bind=engine)
+
+    # produce new session
+    session = Session()
+
+    # print states with 'a'
+    for state in session.query(State).order_by(State.id).filter(State.name == 'a'):
+        print(f"{state.id}: {state.name}")
+
+    # Close session
+    session.close()
