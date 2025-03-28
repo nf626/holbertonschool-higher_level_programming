9324380776459bdd08a65f20e8ffbbf3bb83b734
Nigel Feng <10083@holbertonstudents.com>
2025-03-28 20:00:03 +1100

    0. Creating a Simple Templating Program

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,34 +1,22 @@
-#!/usr/bin/python3
 """ Creating a Simple Templating Program """
 import os
 from string import Template
 
-if __name__ == '__main__':
+def generate_invitations(template, attendees):
+    """generate invites"""
+    
+    try:
+        if isinstance(template, str):
+            print(template)
+    except:
+        raise ValueError("Not string")
 
-    file_path = '/holbertonschool-higher_level_programming/python-server_side_rendering/template.txt'
-
-    def generate_invitations(template, attendees):
-        """generate invites"""
-        
-        path = os.path.exists(file_path)
-        print(path)
-
-        try:
-            if isinstance(template, str):
-                print(template)
-        except:
-            raise TypeError("Not string")
-
-        try:
-            if isinstance(attendees, dict):
-                print(attendees)
-        except:
-            raise TypeError("Not dict")
+    try:
+        if isinstance(attendees, dict):
+            print(attendees)
+    except:
+        raise ValueError("Not dict")
 
 
-        try:
-            temp = Template(template)
-            print(temp.safe_substitute(name="attendees"))
-        except:
-            raise ValueError("Cannot open")
+    #with open(template, 'w', encoding="utf-8") as temp:
 
