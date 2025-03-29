b16c6a144bc3d4bf6574a8849658bfcf65903834
Nigel Feng <10083@holbertonstudents.com>
2025-03-29 14:36:45 +1100

    0. Creating a Simple Templating Program

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -16,6 +16,24 @@ def generate_invitations(template, attendees):
             print(attendees)
     except:
         raise ValueError("Not dict")
+    
+    if not template:
+        return {'error': 'template is empty'}
+    
+    if not attendees:
+        return {'error': 'attendees is empty'}
 
-    with open('output_1.txt', 'r', encoding='utf-8') as file:
-        attendee_list = file.read()
+    for i, attendee in enumerate(attendees, start=1):
+        name = attendee.get("name", "N/A")
+        event_title = attendee.get("event_title", "N/A")
+        event_date = attendee.get("event_date", "N/A")
+        event_location = attendee.get("event_location", "N/A")
+
+        template = template.replace("{name}", name)
+        template = template.replace("{event_title}", event_title)
+        template = template.replace("{event_date}", str(event_date))
+        template = template.replace("{event_location}", event_location)
+
+        with open(f"output_{i}.txt", "w", encoding="utf-8") as f:
+            f.write(template)
+        print(template)
